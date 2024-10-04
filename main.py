ff7d0ae41a0de2b55b58b3ff610cf302d94de89f
linnene <2691234891@qq.com>
2024-10-04 19:22:47 +0800

    text: |   feat:

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import io
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 # 读取配置文件
-with open('./settings.json', 'r', encoding='utf-8') as f:
+with open('./setting.json', 'r', encoding='utf-8') as f:
     settings = json.load(f)
 
 your_username = settings['Set']['NAME']
